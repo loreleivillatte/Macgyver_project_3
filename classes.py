8dae1f76c64dae1506bb3b516175fa6632cf009d
Villate <jaune.pixel@gmail.com>
2018-04-17 14:42:21 +0200

    change frame by structure'class.py'

--- classes.py
+++ classes.py
@@ -18,21 +18,21 @@ class Level:
         """generate level of labyrinth,
         create a general list, containing a list per line to display"""
         with open(self.file) as file:
-            frame_level = []
+            structure_level = []
             for line in file:
                 line_level = []
                 for sprite in line:
                     if sprite != '\n':
                         line_level.append(sprite)
-                frame_level.append(line_level)
-            self.frame = frame_level
+                structure_level.append(line_level)
+            self.structure = structure_level
 
     def display(self, screen):
         """display level depends on structure of list returned by generate"""
         wall = pygame.image.load("pictures/wall.png").convert()
         end = pygame.image.load("pictures/guard.png").convert_alpha()
         num_line = 0
-        for line in self.frame:
+        for line in self.structure:
             num_case = 0
             for sprite in line:
                 sprite_x = num_case * sprite_size
