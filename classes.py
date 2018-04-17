0283de13268688ad62ec8102e9dcb3e2acf04ec6
Villate <jaune.pixel@gmail.com>
2018-04-17 14:28:14 +0200

    add class Player

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-"""classes level"""
+"""class level, Player"""
 
 import pygame
 
@@ -43,3 +43,47 @@ class Level:
                     screen.blit(end, (sprite_x, sprite_y))
                 num_case += 1
             num_line += 1
+
+
+class Player:
+    """character"""
+    def __init__(self, down, left, right, up, level):
+        self.down = pygame.image.load(down).convert_alpha()
+        self.left = pygame.image.load(left).convert_alpha()
+        self.right = pygame.image.load(right).convert_alpha()
+        self.up = pygame.image.load(up).convert_alpha()
+        """starting position of the character"""
+        self.case_x = 7
+        self.case_y = 2
+        self.x = 7 * 40
+        self.y = 2 * 40
+        self.direction = self.down
+        self.level = level
+        
+    def update(self, direction):
+        """ moving the character"""
+        if direction == 'down':
+            if self.case_y < (nb_sprites - 1):
+                """ can't walk on wall"""
+                if self.level.structure[self.case_y+1][self.case_x] != 'w':
+                    self.case_y += 1
+                    self.y = self.case_y * sprite_size
+            self.direction = self.down
+        if direction == 'left':
+            if self.case_x > 0:
+                if self.level.structure[self.case_y][self.case_x-1] != 'w':
+                    self.case_x -= 1
+                    self.x = self.case_x * sprite_size
+            self.direction = self.left
+        if direction == 'right':
+            if self.case_x < (nb_sprites - 1):
+                if self.level.structure[self.case_y][self.case_x+1] != 'w':
+                    self.case_x += 1
+                    self.x = self.case_x * sprite_size
+            self.direction = self.right
+        if direction == 'up':
+            if self.case_y > 0:
+                if self.level.structure[self.case_y-1][self.case_x] != 'w':
+                    self.case_y -= 1
+                    self.y = self.case_y * sprite_size
+            self.direction = self.up
