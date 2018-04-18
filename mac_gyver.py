7b2e2b07364991b360889a0007edf17aa6bf7faf
Villate <jaune.pixel@gmail.com>
2018-04-18 09:39:17 +0200

    add new conditions for victory with items

--- mac_gyver.py
+++ mac_gyver.py
@@ -30,9 +30,26 @@ MAC_GYVER = Player("pictures/mac_down.png", "pictures/mac_left.png",
                    "pictures/mac_right.png", "pictures/mac_up.png", level)
 pygame.key.set_repeat(1, 40)
 
+"""display loot"""
+TUBE_IMG = pygame.image.load(TUBE_POS).convert_alpha()
+NEEDLE_IMG = pygame.image.load(NEEDLE_POS).convert_alpha()
+ETHER_IMG = pygame.image.load(ETHER_POS).convert_alpha()
+
+"""position loot in level"""
+TUBE_POS = Loot(TUBE_IMG, level)
+TUBE_POS.display(TUBE_IMG, screen)
+NEEDLE_POS = Loot(NEEDLE_IMG, level)
+NEEDLE_POS.display(NEEDLE_IMG, screen)
+ETHER_POS = Loot(ETHER_IMG, level)
+ETHER_POS.display(ETHER_IMG, screen)
+
 pygame.display.flip()
 
 GAME_LEVEL = 1
+
+TUBE_NOTPICKED = True
+ETHER_NOTPICKED = True
+NEEDLE_NOTPICKED = True
 
 """main loop"""
 while GAME_LEVEL:
@@ -55,4 +72,27 @@ while GAME_LEVEL:
     screen.fill(pygame.Color('black'))
     level.display(screen)
     screen.blit(MAC_GYVER.direction, (MAC_GYVER.x, MAC_GYVER.y))
+    pygame.display.flip()
+
+    if TUBE_NOTPICKED:
+        screen.blit(TUBE_POS.Loot_Image, (TUBE_POS.x, TUBE_POS.y))
+        if (MAC_GYVER.x, MAC_GYVER .y) == (TUBE_POS.x, TUBE_POS.y):
+            TUBE_NOTPICKED = False
+    if TUBE_NOTPICKED == False:
+        screen.blit(TUBE_POS.Loot_Image, (80, 0))
+        
+    if NEEDLE_NOTPICKED:
+        screen.blit(NEEDLE_POS.Loot_Image, (NEEDLE_POS.x, NEEDLE_POS.y))
+        if (MAC_GYVER.x, MAC_GYVER .y) == (NEEDLE_POS.x, NEEDLE_POS.y):
+            NEEDLE_NOTPICKED = False
+    if NEEDLE_NOTPICKED == False:
+        screen.blit(NEEDLE_POS.Loot_Image, (120, 0))
+        
+    if ETHER_NOTPICKED:
+        screen.blit(ETHER_POS.Loot_Image, (ETHER_POS.x, ETHER_POS.y))
+        if (MAC_GYVER.x, MAC_GYVER .y) == (ETHER_POS.x, ETHER_POS.y):
+            ETHER_NOTPICKED = False
+    if ETHER_NOTPICKED == False:
+        screen.blit(ETHER_POS.Loot_Image, (160, 0))
+        
     pygame.display.flip()
