52829d0a01091c36a57d59f0c9c71bd9d6590bbc
Villate <jaune.pixel@gmail.com>
2018-04-18 09:13:06 +0200

    add path to images to constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,10 @@
+
 """value screen"""
 nb_sprites = 15
 sprite_size = 40
+
+"""pictures for item"""
+TUBE_POS = "pictures/tube.png"
+NEEDLE_POS = "pictures/needle.png"
+ETHER_POS = "pictures/flask.png"
 
